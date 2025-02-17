de46840adb11249f2a7671e2d4c272cd90ad6321
Thomas <thomas.prudhomme@satom.ch>
2025-02-17 15:29:53 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#3b82f6',       // A more gentle blue
-        secondary: '#fcd34d',     // A softer yellow
+        primary: '#5482f3',       // A more gentle blue
+        secondary: '#fdd835',     // A softer yellow
         accent: '#dc2626',        // A more muted red
         neutral: '#6b7280',       // A medium gray for text
         background: '#f5f5f5',    // A light gray for the background
         'primary-dark': '#2563eb',  // Darker shade of primary for hover/active states
       },
       fontFamily: {
-        'sans': ['Roboto', 'sans-serif'],
+        'sans': ['Nunito', 'sans-serif'],
         'serif': ['Merriweather', 'serif'],
       },
     },
