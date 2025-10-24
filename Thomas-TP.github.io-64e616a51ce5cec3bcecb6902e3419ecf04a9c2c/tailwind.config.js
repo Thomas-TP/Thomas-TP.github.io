e6241d0a5728dde6c92f4e1c03e263a8d03a6641
Thomas Prudhomme <thomas.prudhomme@example.com>
2025-10-24 11:21:32 +0200

    Publish all files

---
+++ Thomas-TP.github.io-64e616a51ce5cec3bcecb6902e3419ecf04a9c2c/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4f76f7',
+        secondary: '#fdd835',
+        accent: '#dc2626',
+        neutral: '#374151',
+        'primary-dark': '#2563eb',
+      },
+    },
+  },
+  plugins: [],
+}
